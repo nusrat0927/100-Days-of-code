1c77cb8f4abf7c49276a53a300fc1cefaec3f062
nusrat0927 <40735231+nusrat0927@users.noreply.github.com>
2018-08-05 17:33:37 +0530

    Day-15

---
+++ Code/DAy-15/frequency.py
@@ -0,0 +1,18 @@
+s=input("enter string")
+d={}
+for x in s:
+        if x not in d:
+                d[x]=1
+        else:
+                d[x]+=1
+print(d)
+
+# 2nd Apoproach
+
+s=input("enter string")
+for x in s:
+        print("%s : %d"%(x,s.count(x)))\
+        
+#3rd Approach
+s=input("enter string")
+print({x:s.count(x) for x in s})
