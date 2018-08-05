6d260455fb647f3fb937330cfa2b60bf45ec2ddd
nusrat0927 <40735231+nusrat0927@users.noreply.github.com>
2018-08-05 17:34:20 +0530

    Day-15

---
+++ Code/DAy-15/occurrenceno.py
@@ -0,0 +1,9 @@
+def count(s, c) :
+    res = 0
+    for i in range(len(s)) :   
+            if (s[i] == c):
+            res = res + 1
+    return res
+str= "aaahhabbjkjjkkl"
+c = 'a'
+print(count(str, c))
